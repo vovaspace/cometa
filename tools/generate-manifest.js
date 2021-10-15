cf90c5f11583eacee23e9b8c657cb3d2ef68acbc
Vladimir Lewandowski <vova.space@icloud.com>
2021-10-15 10:18:46 +0300

    Refactor: lib building

---
+++ tools/generate-manifest.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+
+const root = path.resolve();
+const lib = path.join(root, 'lib');
+
+// eslint-disable-next-line import/no-dynamic-require
+const pkg = require(path.join(root, 'package.json'));
+
+const writeJSONSync = (p, content) =>
+  fs.writeFileSync(p, JSON.stringify(content, null, 2));
+
+const fields = (...names) =>
+  names.reduce(
+    (acc, name) => (pkg[name] ? { ...acc, [name]: pkg[name] } : acc),
+    {},
+  );
+
+const manifest = {
+  name: `@cometa/${pkg.name}`,
+  ...fields('version', 'description'),
+  main: `./source/cjs/index.js`,
+  module: `./source/esm/index.js`,
+  typings: `./source/typings/index.d.ts`,
+  sideEffects: false,
+  ...fields(
+    'license',
+    'author',
+    'homepage',
+    'repository',
+    'bugs',
+    'dependencies',
+    'peerDependencies',
+    'keywords',
+  ),
+};
+
+writeJSONSync(path.join(lib, 'package.json'), manifest);
+
+pkg.entrypoints?.forEach((entrypoint) => {
+  const alias = {
+    name: `@cometa/${pkg.name}/${entrypoint}`,
+    main: `../source/cjs/${entrypoint}/index.js`,
+    module: `../source/esm/${entrypoint}/index.js`,
+    typings: `../source/typings/${entrypoint}/index.d.ts`,
+    sideEffects: false,
+  };
+
+  const dist = path.join(lib, entrypoint);
+  fs.mkdirSync(dist);
+
+  writeJSONSync(path.join(dist, 'package.json'), alias);
+});
